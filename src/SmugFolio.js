2e439cfc32ab8c7ad8ac790296ca10684c06e2ad
bytesparallel <jason.ross841@gmail.com>
2022-03-17 22:26:36 -0500

    saving imports on SmugFolio.js

--- src/SmugFolio.js
+++ src/SmugFolio.js
@@ -1,4 +1,16 @@
-
+import { Route } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
 export const SmugFolio = () => {
-    
+
+    if (localStorage.getItem("sf_token")) {
+        return(
+         <>
+            <Route>
+                <NavBar />
+                <ApplicationViews />
+            </Route>
+            </>
+            )
+        }
 }
